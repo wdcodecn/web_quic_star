0d84037705a0f926ba477670f42ff79283f16b8b
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-10-26 17:52:09 +0800

    feature: to_snake_case

--- derive_macro/derive_builder_core/src/lib.rs
+++ derive_macro/derive_builder_core/src/lib.rs
@@ -17,7 +17,7 @@
 
 // #![deny(warnings, missing_docs)]
 #![cfg_attr(test, recursion_limit = "100")]
-
+#![feature(let_chains)]
 #[macro_use]
 extern crate darling;
 extern crate proc_macro;
@@ -63,7 +63,7 @@ pub fn builder_for_struct(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
         }
     };
     let model = opts.ident.clone();
-    let schema = format_ident!("{}s", model.to_string().to_lowercase());
+    let schema = format_ident!("{}s", model.to_string().to_snake_case());
     let new_model = format_ident!("New{}", model);
     let mut builder = opts.as_builder();
     let builder_ident = opts.builder_ident();
@@ -286,4 +286,58 @@ pub fn builder_for_struct(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
     let mut stream = quote!(#builder);
     stream.append_all(f);
     stream.into()
+}
+pub trait ToSnakeCase: AsRef<str> {
+    fn to_snake_case(&self) -> String;
+}
+impl<T> ToSnakeCase for T
+where
+    T: AsRef<str>,
+{
+    fn to_snake_case(&self) -> String {
+        let text = self.as_ref();
+
+        let mut buffer = String::with_capacity(text.len() + text.len() / 2);
+
+        let mut text = text.chars();
+
+        if let Some(first) = text.next() {
+            let mut n2: Option<(bool, char)> = None;
+            let mut n1: (bool, char) = (first.is_lowercase(), first);
+
+            for c in text {
+                let prev_n1 = n1.clone();
+
+                let n3 = n2;
+                n2 = Some(n1);
+                n1 = (c.is_lowercase(), c);
+
+                // insert underscore if acronym at beginning
+                // ABc -> a_bc
+
+                if let Some((false, c3)) = n3
+                    && let Some((false, c2)) = n2
+                    && n1.0
+                    && c3.is_uppercase()
+                    && c2.is_uppercase()
+                {
+                    buffer.push('_');
+                }
+
+                buffer.push_str(&prev_n1.1.to_lowercase().to_string());
+
+                // insert underscore before next word
+                // abC -> ab_c
+                if let Some((true, _)) = n2
+                    && n1.1.is_uppercase()
+                {
+                    buffer.push('_');
+                }
+            }
+
+            buffer.push_str(&n1.1.to_lowercase().to_string());
+        }
+
+        buffer
+    }
 }
