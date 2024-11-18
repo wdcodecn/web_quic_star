ca891f6d6069247aeb94c0b17c4dbc6d115e73be
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-11-18 09:33:20 +0800

    Paginate

--- src/bin/without_db_driver.rs
+++ src/bin/without_db_driver.rs
@@ -1,16 +1,28 @@
-use r2d2_postgres::{postgres::NoTls, PostgresConnectionManager};
+use diesel::{QueryDsl, SelectableHelper};
 use std::env;
-use web_quic_star::db_models::group_permission::GroupsPermission;
+use web_quic_star::db_models::user::User;
+use web_quic_star::db_models::{setup_connection_pool, Paginate};
+use web_quic_star::schema::users::dsl::users;
 use web_quic_star::set_env;
 
 fn main() {
     set_env();
+    let connection_pool = setup_connection_pool();
+    let mut pooled_connection = connection_pool.get().unwrap();
+    let x = users
+        .select(User::as_select())
+        .paginate(1, 10)
+        .load_and_count_pages(&mut pooled_connection)
+        .unwrap();
+
+    println!("{:?}", x);
+
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    let manager = PostgresConnectionManager::new(database_url.parse().unwrap(), NoTls);
-    let pool = r2d2::Pool::new(manager).unwrap();
-    let new = GroupsPermission { group_id: 0, permission_id: 0 };
-    let mut client = pool.get().unwrap();
+    // let manager = PostgresConnectionManager::new(database_url.parse().unwrap(), NoTls);
+    // let pool = r2d2::Pool::new(manager).unwrap();
+    // let new = GroupsPermission { group_id: 0, permission_id: 0 };
+    // let mut client = pool.get().unwrap();
 
     // diesel::insert_into(groups_permissions).values(new).execute(&mut client);
     // for i in 0..10i32 {
--- src/db_models/mod.rs
+++ src/db_models/mod.rs
@@ -1,11 +1,11 @@
-use diesel::r2d2::ConnectionManager;
-use r2d2::Pool;
-use std::env;
-use diesel::{QueryableByName};
 use diesel::prelude::*;
 use diesel::query_builder::*;
 use diesel::query_dsl::methods::LoadQuery;
+use diesel::r2d2::ConnectionManager;
 use diesel::sql_types::BigInt;
+use diesel::QueryableByName;
+use r2d2::Pool;
+use std::env;
 pub mod group;
 pub mod group_permission;
 pub mod permission;
@@ -16,14 +16,14 @@ pub type DbType = diesel::pg::Pg;
 
 pub type ConnPool = Pool<ConnectionManager<Conn>>;
 
+#[cfg(feature = "postgres")]
+pub type Conn = diesel::PgConnection;
+
 #[derive(QueryableByName)]
 pub struct Count {
     #[sql_type = "diesel::sql_types::BigInt"]
     pub count: i64,
 }
-
-#[cfg(feature = "postgres")]
-pub type Conn = diesel::PgConnection;
 
 pub fn setup_connection_pool() -> ConnPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
@@ -37,29 +37,22 @@ pub fn setup_connection_pool() -> ConnPool {
         .expect("Could not build connection pool")
 }
 
-
-
 /// example: let x = groups_permissions.select(GroupsPermission::as_select())
 /// .paginate(3,Some(10)).load_and_count_pages(&mut connection)?;
-pub trait PaginateAndFilterDelete: Sized {
-    fn paginate(self, page_no: i64,page_size:Option<i64>) -> Paginated<Self>;
-    const DEFAULT_PER_PAGE: i64 = 10;
-
+pub trait Paginate: Sized {
+    fn paginate(self, page_no: i64, page_size: i64) -> Paginated<Self>;
 }
 
-impl<T> PaginateAndFilterDelete for T {
-    fn paginate(self, page_no: i64,page_size:Option<i64>) -> Paginated<Self> {
-        let page_size = page_size.unwrap_or(Self::DEFAULT_PER_PAGE);
+impl<T> Paginate for T {
+    fn paginate(self, page_no: i64, page_size: i64) -> Paginated<Self> {
         Paginated {
             query: self,
             per_page: page_size,
             page: page_no,
             offset: (page_no - 1) * page_size,
         }
-
     }
 }
-
 
 #[derive(Debug, Clone, Copy, QueryId)]
 pub struct Paginated<T> {
@@ -70,11 +63,9 @@ pub struct Paginated<T> {
 }
 
 impl<T> Paginated<T> {
-
-
-    pub fn load_and_count_pages<'a, U>(self, conn: &mut PgConnection) -> QueryResult<(Vec<U>, i64)>
+    pub fn load_and_count_pages<'a, U>(self, conn: &mut Conn) -> QueryResult<(Vec<U>, i64)>
     where
-        Self: LoadQuery<'a, PgConnection, (U, i64)>,
+        Self: LoadQuery<'a, Conn, (U, i64)>,
     {
         let per_page = self.per_page;
         let results = self.load::<(U, i64)>(conn)?;
@@ -89,7 +80,7 @@ impl<T: Query> Query for Paginated<T> {
     type SqlType = (T::SqlType, BigInt);
 }
 
-impl<T> RunQueryDsl<PgConnection> for Paginated<T> {}
+impl<T> RunQueryDsl<Conn> for Paginated<T> {}
 
 #[cfg(feature = "postgres")]
 impl<T> QueryFragment<DbType> for Paginated<T>
@@ -99,7 +90,7 @@ where
     fn walk_ast<'b>(&'b self, mut out: AstPass<'_, 'b, DbType>) -> QueryResult<()> {
         out.push_sql("SELECT *, COUNT(1) OVER () FROM (");
         self.query.walk_ast(out.reborrow())?;
-        out.push_sql(") t where t.is_delete = false LIMIT ");
+        out.push_sql(") t  LIMIT ");
         out.push_bind_param::<BigInt, _>(&self.per_page)?;
         out.push_sql(" OFFSET ");
         out.push_bind_param::<BigInt, _>(&self.offset)?;
